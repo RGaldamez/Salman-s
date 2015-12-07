6a4920d39524412e038c647d4d69f09fcc0ced23
Ricardo Galdamez <ricardo.j.galdamez@gmail.com>
2015-12-07 16:10:43 -0600

    avances

---
+++ Salmans/src/salmans/Producto.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salmans;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rick
+ */
+public class Producto {
+    
+    // lista de ingredientes, un tiempo de preparación (medido en segundos), un nombre y un precio.
+    private ArrayList<String> ingredientes;
+    private int tiempo;
+    private String nombre;
+    private int precio;
+
+    public Producto(ArrayList<String> ingredientes, int tiempo, String nombre, int precio) {
+        this.ingredientes = ingredientes;
+        this.tiempo = tiempo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Producto(int tiempo, String nombre, int precio) {
+        this.tiempo = tiempo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.ingredientes = new ArrayList();
+    }
+
+    public ArrayList<String> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<String> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto:" + "Tiempo de Preparacion: " + tiempo + " Nombre: " + nombre + ", Precio: " + precio;
+    }
+    
+    
+    
+    
+    
+}
